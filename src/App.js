0bdd69decd3027e5d645131321c2c44d82ed7b69
Raul <raultorres2603@gmail.com>
2022-05-29 20:40:44 +0200

    SoT Font

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import "./css/main.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import LoginForm from "./components/LoginForm";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <LoginForm />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="col-12">
+          <p className="sotFont titleLogin">The Reaper's Secret</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LoginForm;
