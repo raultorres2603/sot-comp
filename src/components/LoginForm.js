ab25a44e8eb840ea5bf38e9d058d029631b7ffc7
Raul <raultorres2603@gmail.com>
2022-05-29 22:15:20 +0200

    Animations

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { fadeInDown } from "react-animations";
+import Radium, { StyleRoot } from "radium";
 
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      styles: {
+        fadeInDown: {
+          animation: "x 1.5s",
+          animationName: Radium.keyframes(fadeInDown, "fadeInDown"),
+        },
+      },
+    };
   }
 
   render() {
     return (
       <div className="container">
         <div className="col-12">
-          <div className="row mt-4">
-            <p className="sotFont titleLogin">The Reaper's Secret</p>
-          </div>
+          <StyleRoot>
+            <div className="row mt-4" style={this.state.styles.fadeInDown}>
+              <p className="sotFont titleLogin">The Reaper's Secret</p>
+            </div>
+          </StyleRoot>
         </div>
       </div>
     );
