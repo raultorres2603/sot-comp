993448d699d1077ae900c8eb07d94f436ce9720b
Raul <raultorres2603@gmail.com>
2022-05-31 23:14:18 +0200

    Better CSS for LOGIN

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { fadeInDown } from "react-animations";
+import {
+  fadeInDown,
+  fadeInLeft,
+  fadeInRight,
+  fadeInUp,
+} from "react-animations";
 import Radium, { StyleRoot } from "radium";
 
 class LoginForm extends React.Component {
@@ -9,8 +14,20 @@ class LoginForm extends React.Component {
     this.state = {
       styles: {
         fadeInDown: {
-          animation: "x 1.5s",
+          animation: "x 1s",
           animationName: Radium.keyframes(fadeInDown, "fadeInDown"),
+        },
+        fadeInLeft: {
+          animation: "x 1s",
+          animationName: Radium.keyframes(fadeInLeft, "fadeInLeft"),
+        },
+        fadeInRight: {
+          animation: "x 1s",
+          animationName: Radium.keyframes(fadeInRight, "fadeInRight"),
+        },
+        fadeInUp: {
+          animation: "x 1s",
+          animationName: Radium.keyframes(fadeInUp, "fadeInUp"),
         },
       },
     };
@@ -19,10 +36,44 @@ class LoginForm extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="col-12">
+        <div className="col-md-12">
           <StyleRoot>
             <div className="row mt-4" style={this.state.styles.fadeInDown}>
-              <p className="sotFont titleLogin">The Reaper's Secret</p>
+              <p className="titleLogin">Athena's Fortune</p>
+            </div>
+            <div className="row mt-2" style={this.state.styles.fadeInDown}>
+              <p className="formTitle">Join Us</p>
+            </div>
+            <div className="row mt-3">
+              <div className="col-md-4" style={this.state.styles.fadeInLeft}>
+                <input
+                  class="form-control form-control-lg controlSOT"
+                  type="email"
+                  name="email"
+                  placeholder="E-mail"
+                />
+              </div>
+              <div className="col-md-4" style={this.state.styles.fadeInLeft}>
+                <input
+                  class="form-control form-control-lg controlSOT"
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                />
+              </div>
+              <div className="col-md-4" style={this.state.styles.fadeInRight}>
+                <input
+                  class="form-control form-control-lg controlSOT"
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <div className="row mt-5" style={this.state.styles.fadeInUp}>
+              <button type="button" class="btn btn-info btn-lg">
+                Enter
+              </button>
             </div>
           </StyleRoot>
         </div>
